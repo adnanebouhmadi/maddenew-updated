cfda72b2537b9d28a1f6eee11e26e9cd6d1476c2
ADNANE BOUHMADI <adnanebouhmadi@gmail.com>
2026-09-07 19:09:37 +0100

    fix4

--- api/index.php
+++ api/index.php
@@ -1,8 +1,27 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+// إنشاء مجلدات التخزين المؤقتة في /tmp لمنع خطأ الصلاحيات 500
+$storageDirs = [
+    '/tmp/storage/app/public',
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/cache',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/logs',
+    '/tmp/bootstrap/cache',
+];
 
-$app = require_once __DIR__ . '/../bootstrap/app.php';
+foreach ($storageDirs as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0755, true);
+    }
+}
+
+require _DIR_ . '/../vendor/autoload.php';
+
+$app = require_once _DIR_ . '/../bootstrap/app.php';
+
+// توجيه مجلد التخزين إلى /tmp
+$app->useStoragePath('/tmp/storage');
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
